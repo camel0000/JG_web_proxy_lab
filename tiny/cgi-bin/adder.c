152b520d318cfc4863eefebd332b1f59d0848624
camel0000 <camel0000@naver.com>
2023-04-16 11:36:32 +0000

    solved assignment 11.10

--- tiny/cgi-bin/adder.c
+++ tiny/cgi-bin/adder.c
@@ -13,10 +13,15 @@ int main(void) {
     if ((buf = getenv("QUERY_STRING")) != NULL) {
 	p = strchr(buf, '&');
 	*p = '\0';
-	strcpy(arg1, buf);
+
+	/*strcpy(arg1, buf);
 	strcpy(arg2, p+1);
 	n1 = atoi(arg1);
-	n2 = atoi(arg2);
+	n2 = atoi(arg2);*/
+
+    /* 숙제문제 11.10 */
+    sscanf(buf, "first=%d", &n1);       // sscanf() : scanf()와 동일하나, 입력 대상이 표준 입력이 아닌 매개변수로 전달되는 문자열 버퍼
+    sscanf(p + 1, "second=%d", &n2);    // 프로그램 내부에서 생성된 문자열, 파일에서 읽은 문자열을 내부 변수에 저장 후 분리해야 하는 경우에 유용
     }
 
     /* Make the response body */
